da161c7667f793ed1164ab85c5a239bfed3fb182
Caitlin Hines <caitlinhines@me.com>
2017-03-24 13:51:30 -0700

    looks right but will not save answers to database

--- app/models/answer.js
+++ app/models/answer.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   response: DS.attr(),
-  author: DS.attr()
+  author: DS.attr(),
+  question: DS.belongsTo('question', { async: true })
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -5,18 +5,27 @@ export default Ember.Route.extend({
     return this.store.findRecord('question', params.question_id);
   },
   actions: {
-      update(question, params) {
-        Object.keys(params).forEach(function(key) {
-          if(params[key]!==undefined) {
-            question.set(key,params[key]);
-          }
-        });
-        question.save();
-        this.transitionTo('index');
-      },
-      destroyQuestion(question) {
-        question.destroyRecord();
-        this.transitionTo('index');
-      }
+    update(question, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          question.set(key,params[key]);
+        }
+      });
+      question.save();
+      this.transitionTo('index');
+    },
+    destroyQuestion(question) {
+      question.destroyRecord();
+      this.transitionTo('index');
+    },
+    saveAnswer(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      var question = params.question;
+      question.get('answers').addObject(newAnswer);
+      newAnswer.save().then(function() {
+        return question.save();
+      });
+      this.transitionTo('question', question);
     }
-  });
+  }
+});
