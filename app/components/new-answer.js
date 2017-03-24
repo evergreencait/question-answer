623810b403fb1fa2670261f13aed9f1156018374
Caitlin Hines <caitlinhines@me.com>
2017-03-24 15:30:53 -0700

    showing answers on individual question pages

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -9,7 +9,8 @@ export default Ember.Component.extend({
     saveAnswer() {
      var params = {
        response: this.get('response'),
-       author: this.get('author')
+       author: this.get('author'),
+       question: this.get('question')
      };
      this.set('addNewAnswer', false);
      this.sendAction('saveAnswer', params);
